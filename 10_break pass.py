c1d74cd4735a2ec7db0f69ab8994e05d76edbbea
heewoo <leeheewoo@email.com>
2025-09-11 11:20:14 +0900

    continue 문 cnrk

--- 10_break pass.py
+++ 10_break pass.py
@@ -1,11 +1,29 @@
+from platform import system
+
 cup = 0
 running = True
 
-while running:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        break
+# while running:
+#     cup += 1
+#     print(cup)
+#     if cup == 10:
+#         break
 
-print('while 문 종료')
+# print('while 문 종료')
+
+for i in range(1,10):
+   if i ==3:
+       continue
+   print(i)
+
+for i in range(1,10):
+    if i ==(3):
+       continue
+
+    if i ==(6):
+        continue
+
+    if i ==(9):
+        continue
+    print(i)
 
