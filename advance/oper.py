84c6f0919e359396c827877a5a4376a62db0508a
heewoo <leeheewoo@email.com>
2025-09-15 11:45:38 +0900

    구조변경

---
+++ advance/oper.py
@@ -0,0 +1,21 @@
+# 변수
+field = 10
+field = 'String'
+
+# 함수
+def sum(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
+
+
+
+
+
